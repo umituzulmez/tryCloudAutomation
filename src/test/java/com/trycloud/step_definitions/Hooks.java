9e06c9a22bba5ecc2b2c8023f7bca32980fae893
Sukru <skrcakmaktepe@gmail.com>
2020-01-26 13:23:43 +0100

    step def

---
+++ src/test/java/com/trycloud/step_definitions/Hooks.java
@@ -0,0 +1,43 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.sql.Driver;
+
+public class Hooks {
+
+   /* @Before
+   public  void setUp(){
+
+        Driver.get().manage().window().maximize();
+    }
+    @After
+    public void tearDown(Scenario scenario){
+
+        //if the scenerio fails take the screen shot
+        if(scenario.isFailed()){
+            final byte [] screenshot=((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot,"image/png");
+
+        }
+        //Driver.closeDriver();
+    }
+    @After("@store_manager")
+    public void tearDownStoreManager(){
+
+        System.out.println("\tthis is coming from AFTER STORE MANAGER\n");
+    }
+    @After("@db")
+    public void tearDownDatabase(){
+        System.out.println("\tCLOSING DATABASE CONNECTION");
+    }
+    @Before("@db")
+    public void setUpDatabase(){
+        System.out.println("\tCONNECTION DATABASE");
+    }*/
+
+}
