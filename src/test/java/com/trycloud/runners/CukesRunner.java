02695ddf1075a926004acb04d008e7e5218f58f2
MustafaBalci <mbalcilar18@gmail.com>
2020-01-28 00:16:21 +0100

    cukesRunner

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -1,5 +1,17 @@
 package com.trycloud.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src/test/Resources/features/",
+        glue = "com/tryCloud/step_definitions/",
+        dryRun = false
+
+)
 public class CukesRunner {
-    //Test
+
 }
